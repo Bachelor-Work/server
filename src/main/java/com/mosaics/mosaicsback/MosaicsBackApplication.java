760f43592f0444a278e6d125e66f32311bcac5d7
StepanOmeliukh <stepan.omeliukh.knm.2019@lpnu.ua>
2022-12-21 04:12:10 +0200

    Init of server side

---
+++ src/main/java/com/mosaics/mosaicsback/MosaicsBackApplication.java
@@ -0,0 +1,13 @@
+package com.mosaics.mosaicsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosaicsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MosaicsBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mosaics/mosaicsback/MosaicsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mosaics.mosaicsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosaicsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
